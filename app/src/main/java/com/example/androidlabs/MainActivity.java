a364a61ce0e8fc9de11dbe1fb2ebf9f3d9aa9b58
wiam95 <wiam95@hotmail.com>
2020-01-22 19:51:23 -0500

    Added Toast and SnackBar to switch button

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.*;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,8 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,19 +23,35 @@ public class MainActivity extends AppCompatActivity {
     CheckBox cb;
     Switch sw;
     EditText et;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_linear);
 
+        view = findViewById(R.id.main_layout);
+
         btn = (Button)findViewById(R.id.Button);
         cb = (CheckBox)findViewById(R.id.checkBox);
         sw = (Switch)findViewById(R.id.Switch);
         et = (EditText)findViewById(R.id.editText);
 
+        //Checks the checkbox then releases a toast message to user
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                cb.setChecked(isChecked);
+                Toast.makeText(getApplicationContext(),"Here is more information",Toast.LENGTH_LONG).show();
+
+                if(isChecked == true)
+                    Snackbar.make(view, "The switch is now on",2000).show();
+                if(isChecked == false)
+                    Snackbar.make(view, "Off",2000).show();
+            }
+        });
+
+
     }
-
-
 
 }
